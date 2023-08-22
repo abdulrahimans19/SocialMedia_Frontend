5c89c18645d8dbf9394028d13ae41a3f6c3f607d
abdulrahimans19 <117072532+abdulrahimans19@users.noreply.github.com>
2023-08-22 21:07:49 +0530

    updated

--- src/scenes/widgets/UserWidget.jsx
+++ src/scenes/widgets/UserWidget.jsx
@@ -22,7 +22,7 @@ import {
     const main = palette.neutral.main;
   
     const getUser = async () => {
-      const response = await fetch(`http://localhost:3001/users/${userId}`, {
+      const response = await fetch(`https://socialmedia-1o42.onrender.com/${userId}`, {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
       });
